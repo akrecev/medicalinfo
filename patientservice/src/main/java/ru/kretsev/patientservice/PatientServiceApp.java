6da05403b0afb65e563d915ab34107d1f3ca1c7c
akrecev <akrecev@gmail.com>
2025-02-19 21:35:28 +0300

    fix docker settings

---
+++ patientservice/src/main/java/ru/kretsev/patientservice/PatientServiceApp.java
@@ -0,0 +1,13 @@
+package ru.kretsev.patientservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@EnableAsync
+@SpringBootApplication
+public class PatientServiceApp {
+    public static void main(String[] args) {
+        SpringApplication.run(PatientServiceApp.class, args);
+    }
+}
