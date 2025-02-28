ecb322615a66ba1b8bb565456bce2c095b99f043
akrecev <akrecev@gmail.com>
2025-02-28 20:22:51 +0300

    add swagger description on patient controller

--- patientservice/src/main/java/ru/kretsev/patientservice/controller/PatientController.java
+++ patientservice/src/main/java/ru/kretsev/patientservice/controller/PatientController.java
@@ -1,5 +1,8 @@
 package ru.kretsev.patientservice.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -15,11 +18,25 @@ import ru.kretsev.patientservice.service.PatientService;
 public class PatientController {
     private final PatientService patientService;
 
+    @Operation(
+            summary = "Create a new patient",
+            description = "Creates a new patient record and returns the created patient details")
+    @ApiResponses(
+            value = {
+                @ApiResponse(responseCode = "201", description = "Patient created successfully"),
+                @ApiResponse(responseCode = "400", description = "Invalid input data")
+            })
     @PostMapping
     public ResponseEntity<PatientResponseDTO> createPatient(@Valid @RequestBody PatientDTO patientDTO) {
         return new ResponseEntity<>(patientService.create(patientDTO), HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Get patient details", description = "Retrieves details of a patient by ID")
+    @ApiResponses(
+            value = {
+                @ApiResponse(responseCode = "200", description = "Patient found"),
+                @ApiResponse(responseCode = "404", description = "Patient not found")
+            })
     @GetMapping("/{id}")
     public ResponseEntity<PatientResponseDTO> getPatient(@PathVariable Long id) {
         return ResponseEntity.ok(patientService.getById(id));
